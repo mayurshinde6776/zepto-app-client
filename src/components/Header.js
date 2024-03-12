604e59f23bf298052ffb17430a3d5e14fe89a646
Mayur-Thinkbridge <mayurshinde6776@gmail.com>
2024-03-12 16:47:23 +0530

    changes POST API path , added hosted path

--- src/components/Header.js
+++ src/components/Header.js
@@ -77,7 +77,7 @@ const Header = () => {
     const headers = {
       "Content-Type": "application/json"
     }
-    const response = await fetch('http://localhost:7000/api/create-checkout-session', {
+    const response = await fetch('https://zepto-app-server.onrender.com/api/create-checkout-session', {
       method: "POST",
       headers: headers,
       body: JSON.stringify(body)
